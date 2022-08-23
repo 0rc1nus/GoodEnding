afed3ec1d7aa57271b2dec0d479375db837fabc5
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-08-23 17:46:58 +0200

    Wildflowers renewability

--- src/main/java/net/orcinus/goodending/blocks/WildflowersBlock.java
+++ src/main/java/net/orcinus/goodending/blocks/WildflowersBlock.java
@@ -3,14 +3,19 @@ package net.orcinus.goodending.blocks;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
 import net.minecraft.block.PlantBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
-public class WildflowersBlock extends PlantBlock {
+public class WildflowersBlock extends PlantBlock implements Fertilizable {
     protected static final VoxelShape SHAPE = Block.createCuboidShape(2, 0, 2, 14, 6, 14);
 
     public WildflowersBlock(AbstractBlock.Settings settings) {
@@ -18,9 +23,50 @@ public class WildflowersBlock extends PlantBlock {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         Vec3d vec3d = state.getModelOffset(world, pos);
         return SHAPE.offset(vec3d.x, vec3d.y, vec3d.z);
+    }
+
+    @Override
+    public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        if (world.getBlockState(pos.down()).isIn(BlockTags.DIRT)) {
+            int j = 1;
+            int l = 0;
+            int m = pos.getX() - 2;
+            int n = 0;
+
+            for (int o = 0; o < 5; ++o) {
+                for (int p = 0; p < j; ++p) {
+                    int q = 2 + pos.getY();
+                    for (int r = q - 2; r < q; ++r) {
+                        BlockPos blockPos = new BlockPos(m + o, r, pos.getZ() - n + p);
+                        if (blockPos == pos || random.nextInt(4) != 0  || !world.getBlockState(blockPos.down()).isIn(BlockTags.DIRT) || !world.getBlockState(blockPos).isAir()) continue;
+                        world.setBlockState(blockPos, state, Block.NOTIFY_ALL);
+                    }
+                }
+                if (l < 2) {
+                    j += 2;
+                    ++n;
+                } else {
+                    j -= 2;
+                    --n;
+                }
+                ++l;
+            }
+            world.setBlockState(pos, state, Block.NOTIFY_LISTENERS);
+        }
     }
 }
 
