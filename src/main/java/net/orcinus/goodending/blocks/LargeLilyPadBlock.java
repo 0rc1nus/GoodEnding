77dd61354f2793668c5cf18527b6d90745be43a8
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-09-10 03:30:48 +0200

    Large lily pad renewability

--- src/main/java/net/orcinus/goodending/blocks/LargeLilyPadBlock.java
+++ src/main/java/net/orcinus/goodending/blocks/LargeLilyPadBlock.java
@@ -2,6 +2,7 @@ package net.orcinus.goodending.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
 import net.minecraft.block.PlantBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
@@ -14,15 +15,18 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
+import net.orcinus.goodending.init.GoodEndingBlocks;
 
 @SuppressWarnings("deprecation")
-public class LargeLilyPadBlock extends PlantBlock implements Waterloggable {
+public class LargeLilyPadBlock extends PlantBlock implements Waterloggable, Fertilizable {
     protected static final VoxelShape SHAPE = Block.createCuboidShape(0, 15, 0, 16, 16, 16);
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
@@ -55,6 +59,45 @@ public class LargeLilyPadBlock extends PlantBlock implements Waterloggable {
         return (world.getFluidState(pos.up()).isOf(Fluids.WATER) && world.getFluidState(pos.up(2)).isEmpty());
     }
 
+    @Override
+    public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        if (world.getBlockState(pos).getFluidState().isOf(Fluids.WATER)) {
+            int j = 1;
+            int l = 0;
+            int m = pos.getX() - 2;
+            int n = 0;
+
+            for (int o = 0; o < 5; ++o) {
+                for (int p = 0; p < j; ++p) {
+                    int q = 2 + pos.getY();
+                    for (int r = q - 2; r < q; ++r) {
+                        BlockPos blockPos = new BlockPos(m + o, r, pos.getZ() - n + p);
+                        if (blockPos == pos || random.nextInt(4) != 0 || world.getBlockState(blockPos).getBlock().getDefaultState().isOf(GoodEndingBlocks.LARGE_LILY_PAD)  || !world.getBlockState(pos).getFluidState().isOf(Fluids.WATER) || !world.getBlockState(blockPos).getFluidState().isOf(Fluids.WATER)) continue;
+                        world.setBlockState(blockPos, state, Block.NOTIFY_ALL);
+                    }
+                }
+                if (l < 2) {
+                    j += 2;
+                    ++n;
+                } else {
+                    j -= 2;
+                    --n;
+                }
+                ++l;
+            }
+            world.setBlockState(pos, state, Block.NOTIFY_LISTENERS);
+        }
+    }
 
     @Override
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
