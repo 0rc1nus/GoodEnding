56e047c2add47a582bbc83d38d397864fb43cd32
0rc1nus <mitchellctlui@gmail.com>
2022-09-11 20:35:24 +0800

    Fancy Oak Check

--- src/main/java/net/orcinus/goodending/init/GoodEndingWorldGen.java
+++ src/main/java/net/orcinus/goodending/init/GoodEndingWorldGen.java
@@ -29,6 +29,7 @@ import net.minecraft.world.gen.feature.SimpleBlockFeatureConfig;
 import net.minecraft.world.gen.feature.SimpleRandomFeatureConfig;
 import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.TreePlacedFeatures;
 import net.minecraft.world.gen.feature.VegetationConfiguredFeatures;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
 import net.minecraft.world.gen.feature.size.ThreeLayersFeatureSize;
@@ -125,12 +126,12 @@ public class GoodEndingWorldGen {
     public static final RegistryEntry<PlacedFeature> PATCH_PINK_FLOWERED_LILY_PLACED = register("patch_pink_flowered_lily_placed", PATCH_PINK_FLOWERED_LILY, VegetationPlacedFeatures.modifiers(2));
 
     public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PATCH_FERN = register("patch_fern", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.FERN))));
-    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PATCH_LARGE_FERN = register("patch_large_fern", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.LARGE_FERN)), List.of(), 32));
-    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> OAK_HAMMOCK_PATCH_TALL_GRASS = register("oak_hammock_patch_tall_grass", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.TALL_GRASS)), List.of(), 30));
+    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> PATCH_LARGE_FERN = register("patch_large_fern", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.LARGE_FERN)), List.of(), 20));
+    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> OAK_HAMMOCK_PATCH_TALL_GRASS = register("oak_hammock_patch_tall_grass", Feature.RANDOM_PATCH, ConfiguredFeatures.createRandomPatchFeatureConfig(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(Blocks.TALL_GRASS)), List.of(), 20));
     public static final RegistryEntry<PlacedFeature> PATCH_TALL_GRASS_PLACED = register("patch_tall_grass_placed", VegetationConfiguredFeatures.PATCH_TALL_GRASS, VegetationPlacedFeatures.modifiers(3));
-    public static final RegistryEntry<PlacedFeature> PATCH_FERN_PLACED = register("patch_fern_placed", PATCH_FERN, VegetationPlacedFeatures.modifiers(3));
-    public static final RegistryEntry<PlacedFeature> PATCH_LARGE_FERN_PLACED = register("patch_large_fern_placed", PATCH_LARGE_FERN, VegetationPlacedFeatures.modifiers(3));
-    public static final RegistryEntry<PlacedFeature> OAK_HAMMOCK_PATCH_TALL_GRASS_PLACED = register("oak_hammock_patch_tall_grass_placed", OAK_HAMMOCK_PATCH_TALL_GRASS, VegetationPlacedFeatures.modifiers(3));
+    public static final RegistryEntry<PlacedFeature> PATCH_FERN_PLACED = register("patch_fern_placed", PATCH_FERN, VegetationPlacedFeatures.modifiers(1));
+    public static final RegistryEntry<PlacedFeature> PATCH_LARGE_FERN_PLACED = register("patch_large_fern_placed", PATCH_LARGE_FERN, VegetationPlacedFeatures.modifiers(2));
+    public static final RegistryEntry<PlacedFeature> OAK_HAMMOCK_PATCH_TALL_GRASS_PLACED = register("oak_hammock_patch_tall_grass_placed", OAK_HAMMOCK_PATCH_TALL_GRASS, VegetationPlacedFeatures.modifiers(2));
 
     public static final RegistryEntry<ConfiguredFeature<DefaultFeatureConfig, ?>> LARGE_LILY_PADS = register("large_lily_pads", GoodEndingFeatures.LARGE_LILY_PADS, new DefaultFeatureConfig());
 
--- src/main/java/net/orcinus/goodending/world/gen/features/FancyDarkOakFeature.java
+++ src/main/java/net/orcinus/goodending/world/gen/features/FancyDarkOakFeature.java
@@ -40,97 +40,103 @@ public class FancyDarkOakFeature extends Feature<DefaultFeatureConfig> {
         if (!world.getBlockState(blockPos.down()).isIn(BlockTags.DIRT)) {
             return false;
         } else {
-            blockPos = this.getInitialPos(world, blockPos, baseRadius);
-            for (int x = -baseRadius; x <= baseRadius; x++) {
-                for (int z = -baseRadius; z <= baseRadius; z++) {
-                    int height = 7;
-                    if (random.nextInt(3) == 0) {
-                        height *= 1.5;
-                    }
-                    for (int y = 0; y < height; y++) {
-                        BlockPos pos = new BlockPos(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
-                        boolean flag = x == -baseRadius || x == baseRadius;
-                        boolean flag1 = z == -baseRadius || z == baseRadius;
-                        if (!(flag && flag1)) {
-                            boolean logFlag = ((x == 1 && flag1) || (x == -1 && flag1) || (z == 1 && flag) || (z == -1 && flag)) && y > 1;
-                            boolean logFlag1 = ((x == 0 && flag1) || (z == 0 && flag)) && y > 2;
-                            boolean logFlag2 = (x == 1 || x == -1) && (z == 1 || z == -1) && y > 3;
-                            if (logFlag || logFlag1 || logFlag2) continue;
-                            if (y > 5 && ((x == -1 && z == 0) || (x == 1 && z == 0) || (x == 0 && z == -1) || (x == 0 && z == 1))) {
-                                for (Direction direction : Direction.Type.HORIZONTAL) {
-                                    BlockPos branchPos = blockPos.up(height - 1).offset(direction);
-                                    final int length = UniformIntProvider.create(1, 3).get(random);
-                                    for (int i = 0; i < length; i++) {
-                                        if (world.isAir(branchPos.offset(direction, i)) || world.getBlockState(branchPos.offset(direction, i)).getMaterial().isReplaceable()) {
-                                            world.setBlockState(branchPos.offset(direction, i), Blocks.DARK_OAK_LOG.getDefaultState().with(PillarBlock.AXIS, direction.getAxis()), 2);
-                                            for (int t = 0; t < length; t++) {
-                                                if (world.isAir(branchPos.offset(direction, length).up(t)) || world.getBlockState(branchPos.offset(direction, length).up(t)).getMaterial().isReplaceable()) {
-                                                    world.setBlockState(branchPos.offset(direction, length).up(t), Blocks.DARK_OAK_LOG.getDefaultState(), 2);
-                                                    list.add(branchPos.offset(direction, length).up(length));
+            BlockPos initialPos = this.getInitialPos(world, blockPos, baseRadius, 1);
+            if (initialPos == null) {
+                return false;
+            } else {
+                blockPos = initialPos;
+                for (int x = -baseRadius; x <= baseRadius; x++) {
+                    for (int z = -baseRadius; z <= baseRadius; z++) {
+                        int height = 7;
+                        for (int y = 0; y < height; y++) {
+                            BlockPos pos = new BlockPos(blockPos.getX() + x, blockPos.getY() + y, blockPos.getZ() + z);
+                            boolean flag = x == -baseRadius || x == baseRadius;
+                            boolean flag1 = z == -baseRadius || z == baseRadius;
+                            if (!(flag && flag1)) {
+                                boolean logFlag = ((x == 1 && flag1) || (x == -1 && flag1) || (z == 1 && flag) || (z == -1 && flag)) && y > 1;
+                                boolean logFlag1 = ((x == 0 && flag1) || (z == 0 && flag)) && y > 2;
+                                boolean logFlag2 = (x == 1 || x == -1) && (z == 1 || z == -1) && y > 3;
+                                if (logFlag || logFlag1 || logFlag2) continue;
+                                if (y > 5 && ((x == -1 && z == 0) || (x == 1 && z == 0) || (x == 0 && z == -1) || (x == 0 && z == 1))) {
+                                    for (Direction direction : Direction.Type.HORIZONTAL) {
+                                        BlockPos branchPos = blockPos.up(height - 1).offset(direction);
+                                        final int length = UniformIntProvider.create(1, 3).get(random);
+                                        for (int i = 0; i < length; i++) {
+                                            if (world.isAir(branchPos.offset(direction, i)) || world.getBlockState(branchPos.offset(direction, i)).getMaterial().isReplaceable()) {
+                                                world.setBlockState(branchPos.offset(direction, i), Blocks.DARK_OAK_LOG.getDefaultState().with(PillarBlock.AXIS, direction.getAxis()), 2);
+                                                for (int t = 0; t < length; t++) {
+                                                    if (world.isAir(branchPos.offset(direction, length).up(t)) || world.getBlockState(branchPos.offset(direction, length).up(t)).getMaterial().isReplaceable()) {
+                                                        world.setBlockState(branchPos.offset(direction, length).up(t), Blocks.DARK_OAK_LOG.getDefaultState(), 2);
+                                                        list.add(branchPos.offset(direction, length).up(length));
+                                                    }
                                                 }
                                             }
                                         }
                                     }
                                 }
-                            }
-                            if (world.isAir(pos) || world.getBlockState(pos).getMaterial().isReplaceable()) {
-                                world.setBlockState(pos, Blocks.DARK_OAK_LOG.getDefaultState(), 19);
-                            }
-                        }
-                    }
-                }
-            }
-            for (BlockPos pos : list) {
-                int leaveRadius = 2;
-                for (int x = -leaveRadius; x <= leaveRadius; x++) {
-                    for (int z = -leaveRadius; z <= leaveRadius; z++) {
-                        for (int y = -1; y <= 1; y++) {
-                            BlockPos leavePos = new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
-                            boolean flag = (x == -leaveRadius || x == leaveRadius) && (z == -leaveRadius || z == leaveRadius);
-                            boolean flag1 = y == 1 && !(x > -leaveRadius + random.nextInt(2) && x < leaveRadius - random.nextInt(2) && z > -leaveRadius + random.nextInt(2) && z < leaveRadius - random.nextInt(2));
-                            boolean flag2 = y == -1 && !(x * x + z * z <= 4);
-                            if (flag || flag1 || flag2) continue;
-                            if (world.testBlockState(leavePos, DripstoneHelper::canGenerate)) {
-                                world.setBlockState(leavePos, Blocks.DARK_OAK_LEAVES.getDefaultState().with(LeavesBlock.DISTANCE, 1), leaveRadius);
-                            }
-                            if (random.nextFloat() < 0.15F) {
-                                leavePoses.add(leavePos);
+                                if (world.isAir(pos) || world.getBlockState(pos).getMaterial().isReplaceable()) {
+                                    world.setBlockState(pos, Blocks.DARK_OAK_LOG.getDefaultState(), 19);
+                                }
                             }
                         }
                     }
                 }
-            }
-            for (BlockPos pos : leavePoses) {
-                if (random.nextFloat() < 0.5F && world.testBlockState(pos.down(), AbstractBlock.AbstractBlockState::isAir) && world.testBlockState(pos, blockState -> blockState.isOf(Blocks.DARK_OAK_LEAVES))) {
-                    int height = MathHelper.nextInt(random, 1, 6);
-                    for (int i = 0; i <= height; i++) {
-                        BlockPos placePos = pos.down(i);
-                        if (i == height) {
-                            if (world.testBlockState(placePos, AbstractBlock.AbstractBlockState::isAir)) {
+                for (BlockPos pos : list) {
+                    int leaveRadius = 2;
+                    for (int x = -leaveRadius; x <= leaveRadius; x++) {
+                        for (int z = -leaveRadius; z <= leaveRadius; z++) {
+                            for (int y = -1; y <= 1; y++) {
+                                BlockPos leavePos = new BlockPos(pos.getX() + x, pos.getY() + y, pos.getZ() + z);
+                                boolean flag = (x == -leaveRadius || x == leaveRadius) && (z == -leaveRadius || z == leaveRadius);
+                                boolean flag1 = y == 1 && !(x > -leaveRadius + random.nextInt(2) && x < leaveRadius - random.nextInt(2) && z > -leaveRadius + random.nextInt(2) && z < leaveRadius - random.nextInt(2));
+                                boolean flag2 = y == -1 && !(x * x + z * z <= 4);
+                                if (flag || flag1 || flag2) continue;
+                                if (world.testBlockState(leavePos, DripstoneHelper::canGenerate)) {
+                                    world.setBlockState(leavePos, Blocks.DARK_OAK_LEAVES.getDefaultState().with(LeavesBlock.DISTANCE, 1), leaveRadius);
+                                }
+                                if (random.nextFloat() < 0.15F) {
+                                    leavePoses.add(leavePos);
+                                }
+                            }
+                        }
+                    }
+                }
+                for (BlockPos pos : leavePoses) {
+                    if (random.nextFloat() < 0.5F && world.testBlockState(pos.down(), AbstractBlock.AbstractBlockState::isAir) && world.testBlockState(pos, blockState -> blockState.isOf(Blocks.DARK_OAK_LEAVES))) {
+                        int height = MathHelper.nextInt(random, 1, 6);
+                        for (int i = 0; i <= height; i++) {
+                            BlockPos placePos = pos.down(i);
+                            if (i == height) {
+                                if (world.testBlockState(placePos, AbstractBlock.AbstractBlockState::isAir)) {
+                                    world.setBlockState(placePos, GoodEndingBlocks.HANGING_DARK_OAK_LEAVES.getDefaultState(), 2);
+                                    break;
+                                }
+                            }
+                            if (world.getBlockState(placePos.down()).isIn(BlockTags.LOGS)) {
                                 world.setBlockState(placePos, GoodEndingBlocks.HANGING_DARK_OAK_LEAVES.getDefaultState(), 2);
                                 break;
                             }
-                        }
-                        if (world.getBlockState(placePos.down()).isIn(BlockTags.LOGS)) {
-                            world.setBlockState(placePos, GoodEndingBlocks.HANGING_DARK_OAK_LEAVES.getDefaultState(), 2);
-                            break;
-                        }
-                        if (world.testBlockState(placePos, AbstractBlock.AbstractBlockState::isAir)) {
-                            world.setBlockState(placePos, GoodEndingBlocks.HANGING_DARK_OAK_LEAVES_PLANT.getDefaultState(), 2);
+                            if (world.testBlockState(placePos, AbstractBlock.AbstractBlockState::isAir)) {
+                                world.setBlockState(placePos, GoodEndingBlocks.HANGING_DARK_OAK_LEAVES_PLANT.getDefaultState(), 2);
+                            }
                         }
                     }
                 }
+                return true;
             }
-            return true;
         }
     }
 
-    public BlockPos getInitialPos(StructureWorldAccess world, BlockPos originPos, int baseRadius) {
+    public BlockPos getInitialPos(StructureWorldAccess world, BlockPos originPos, int baseRadius, int count) {
         for (int x = -baseRadius; x <= baseRadius; x++) {
             for (int z = -baseRadius; z <= baseRadius; z++) {
                 BlockPos pos = new BlockPos(originPos.getX() + x, originPos.getY(), originPos.getZ() + z);
                 if (world.isAir(pos.down())) {
-                    return this.getInitialPos(world, originPos.down(), baseRadius);
+                    if (count > 4) {
+                        return null;
+                    }
+                    count++;
+                    return this.getInitialPos(world, originPos.down(), baseRadius, count);
                 }
             }
         }
