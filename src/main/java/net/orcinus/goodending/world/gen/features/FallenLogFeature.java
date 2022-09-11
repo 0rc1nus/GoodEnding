09569891afc88adedc74477d73540301b8ad34ac
0rc1nus <mitchellctlui@gmail.com>
2022-09-12 07:45:37 +0800

    Fallen log config change

--- src/main/java/net/orcinus/goodending/world/gen/features/FallenLogFeature.java
+++ src/main/java/net/orcinus/goodending/world/gen/features/FallenLogFeature.java
@@ -35,32 +35,33 @@ public class FallenLogFeature extends Feature<FallenLogConfig> {
         int logLength = MathHelper.nextInt(random, 3, 6);
         BlockPos.Mutable mut = blockPos.mutableCopy();
         List<BlockPos> decorationPoses = Lists.newArrayList();
+        FallenLogConfig config = context.getConfig();
         for (int i = 0; i <= logLength; i++) {
             boolean flag = world.getBlockState(mut).getMaterial().isReplaceable() || world.testBlockState(mut, state -> state.isAir() || state.isOf(Blocks.WATER) || state.isIn(BlockTags.FLOWERS));
             if (world.getBlockState(mut.down()).getMaterial().isReplaceable() || world.testBlockState(mut.down(), DripstoneHelper::canGenerate)) {
                 mut.move(Direction.DOWN);
                 if (flag) {
-                    world.setBlockState(mut, context.getConfig().log.getBlockState(random, mut.toImmutable()).with(PillarBlock.AXIS, direction.getAxis()), 2);
+                    world.setBlockState(mut, config.log.getBlockState(random, mut.toImmutable()).with(PillarBlock.AXIS, direction.getAxis()), 2);
                     decorationPoses.add(mut.toImmutable());
                 }
             }
             if (flag) {
-                world.setBlockState(mut, context.getConfig().log.getBlockState(random, mut.toImmutable()).with(PillarBlock.AXIS, direction.getAxis()), 2);
+                world.setBlockState(mut, config.log.getBlockState(random, mut.toImmutable()).with(PillarBlock.AXIS, direction.getAxis()), 2);
                 decorationPoses.add(mut.toImmutable());
             }
             mut.move(direction);
         }
         for (BlockPos pos : decorationPoses) {
-            if (world.getBlockState(pos.up()).isAir() && random.nextInt(5) == 0 && context.getConfig().brown_mushroom) {
+            if (world.getBlockState(pos.up()).isAir() && random.nextInt(5) == 0 && config.brown_mushroom) {
                 world.setBlockState(pos.up(), Blocks.BROWN_MUSHROOM.getDefaultState(), 2);
             }
             for (Direction directions : Direction.Type.HORIZONTAL) {
-                if (world.getBlockState(pos.offset(directions)).isAir() && random.nextInt(5) == 0) {
+                if (world.getBlockState(pos.offset(directions)).isAir() && random.nextInt(5) == 0 && config.vines) {
                     world.setBlockState(pos.offset(directions), Blocks.VINE.getDefaultState().with(VineBlock.getFacingProperty(directions.getOpposite()), true), 2);
                 }
             }
             Direction dir = Direction.Type.HORIZONTAL.random(random);
-            if (world.isAir(pos.offset(dir)) && random.nextInt(3) == 0 && context.getConfig().shelf_mushroom) {
+            if (world.isAir(pos.offset(dir)) && random.nextInt(3) == 0 && config.shelf_mushroom) {
                 world.setBlockState(pos.offset(dir), GoodEndingBlocks.BIRCH_MUSHROOM.getDefaultState().with(BirchMushroomPlantBlock.FACING, dir), 2);
             }
         }
--- src/main/java/net/orcinus/goodending/world/gen/features/config/FallenLogConfig.java
+++ src/main/java/net/orcinus/goodending/world/gen/features/config/FallenLogConfig.java
@@ -8,17 +8,23 @@ import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public class FallenLogConfig implements FeatureConfig {
 
-    public static final Codec<FallenLogConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(BlockStateProvider.TYPE_CODEC.fieldOf("log").forGetter(config -> config.log), IntProvider.VALUE_CODEC.fieldOf("length").forGetter(config -> config.length), Codec.BOOL.fieldOf("shelf_mushroom").forGetter(config -> config.shelf_mushroom), Codec.BOOL.fieldOf("brown_mushroom").forGetter(config -> config.brown_mushroom)).apply(instance, FallenLogConfig::new));
+    public static final Codec<FallenLogConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(BlockStateProvider.TYPE_CODEC.fieldOf("log").forGetter(config -> config.log), IntProvider.VALUE_CODEC.fieldOf("length").forGetter(config -> config.length), Codec.BOOL.fieldOf("shelf_mushroom").forGetter(config -> config.shelf_mushroom), Codec.BOOL.fieldOf("brown_mushroom").forGetter(config -> config.brown_mushroom), Codec.BOOL.fieldOf("vines").forGetter(config -> config.vines)).apply(instance, FallenLogConfig::new));
 
     public final BlockStateProvider log;
     public final IntProvider length;
     public final boolean shelf_mushroom;
     public final boolean brown_mushroom;
+    public final boolean vines;
 
     public FallenLogConfig(BlockStateProvider log, IntProvider length, boolean shelf_mushroom, boolean brown_mushroom) {
+        this(log, length, shelf_mushroom, brown_mushroom, true);
+    }
+
+    public FallenLogConfig(BlockStateProvider log, IntProvider length, boolean shelf_mushroom, boolean brown_mushroom, boolean vines) {
         this.log = log;
         this.length = length;
         this.shelf_mushroom = shelf_mushroom;
         this.brown_mushroom = brown_mushroom;
+        this.vines = vines;
     }
 }
