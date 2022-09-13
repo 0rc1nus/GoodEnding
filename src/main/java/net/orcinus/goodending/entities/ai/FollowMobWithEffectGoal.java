b4624e31843f5408ea5c18dfb88a79b57c8f3345
0rc1nus <mitchellctlui@gmail.com>
2022-09-13 08:38:42 +0800

    fix the marsh teleportation (it shouldnt teleport)

--- src/main/java/net/orcinus/goodending/entities/ai/FollowMobWithEffectGoal.java
+++ src/main/java/net/orcinus/goodending/entities/ai/FollowMobWithEffectGoal.java
@@ -1,12 +1,7 @@
 package net.orcinus.goodending.entities.ai;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
-import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
-import net.minecraft.entity.ai.pathing.PathNodeType;
-import net.minecraft.util.math.BlockPos;
 import net.orcinus.goodending.entities.MarshEntity;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,53 +35,8 @@ public class FollowMobWithEffectGoal extends Goal {
 
     @Override
     public void tick() {
-        if (this.marsh.squaredDistanceTo(this.entity) >= 144.0) {
-            this.tryTeleport();
-        } else {
-            this.marsh.getLookControl().lookAt(this.entity, 10.0f, this.marsh.getMaxLookPitchChange());
-            this.marsh.getNavigation().startMovingTo(this.entity, 1.2D);
-        }
-    }
-
-    private void tryTeleport() {
-        BlockPos blockPos = this.entity.getBlockPos();
-        for (int i = 0; i < 10; ++i) {
-            int j = this.getRandomInt(-3, 3);
-            int k = this.getRandomInt(-1, 1);
-            int l = this.getRandomInt(-3, 3);
-            boolean bl = this.tryTeleportTo(blockPos.getX() + j, blockPos.getY() + k, blockPos.getZ() + l);
-            if (!bl) continue;
-            return;
-        }
-    }
-
-    private boolean tryTeleportTo(int x, int y, int z) {
-        if (Math.abs((double)x - this.entity.getX()) < 2.0 && Math.abs((double)z - this.entity.getZ()) < 2.0) {
-            return false;
-        }
-        if (!this.canTeleportTo(new BlockPos(x, y, z))) {
-            return false;
-        }
-        this.marsh.refreshPositionAndAngles((double)x + 0.5, y, (double)z + 0.5, this.marsh.getYaw(), this.marsh.getPitch());
-        this.marsh.getNavigation().stop();
-        return true;
-    }
-
-    private boolean canTeleportTo(BlockPos pos) {
-        PathNodeType pathNodeType = LandPathNodeMaker.getLandNodeType(this.marsh.world, pos.mutableCopy());
-        if (pathNodeType != PathNodeType.WALKABLE) {
-            return false;
-        }
-        BlockState blockState = this.marsh.world.getBlockState(pos.down());
-        if (blockState.getBlock() instanceof LeavesBlock) {
-            return false;
-        }
-        BlockPos blockPos = pos.subtract(this.marsh.getBlockPos());
-        return this.marsh.world.isSpaceEmpty(this.marsh, this.marsh.getBoundingBox().offset(blockPos));
-    }
-
-    private int getRandomInt(int min, int max) {
-        return this.marsh.getRandom().nextInt(max - min + 1) + min;
+        this.marsh.getLookControl().lookAt(this.entity, 10.0f, this.marsh.getMaxLookPitchChange());
+        this.marsh.getNavigation().startMovingTo(this.entity, 1.2D);
     }
 
     @Nullable
